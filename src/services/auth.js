e27b740fb4fde188bf34a9dd27d53355ab094248
Allyson Ramos <136084747+allysonramos-blibp@users.noreply.github.com>
2026-05-24 20:50:15 -0300

    Update auth.js

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: '/'
+    baseURL: import.meta.env.VITE_API_URL || 'https://projeto-pai-o-api.onrender.com'
 });
 
 api.interceptors.request.use((config) => {
